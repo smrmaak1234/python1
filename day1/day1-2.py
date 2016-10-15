e3e31951c03a7e86d42971fd281784c5578210a8
smrmaak1234 <jt0479@naver.com>
2016-10-15 15:07:52 +0900

    1일차 파이썬

---
+++ day1/day1-2.py
@@ -0,0 +1,63 @@
+import turtle 
+
+
+
+
+  
+
+a = turtle.Turtle()
+
+
+
+
+
+
+a.circle(100)
+a.right(90)
+a.penup()
+a.forward(50)
+a.pendown()
+a.left(90)
+
+a.circle(150)
+
+a.left(90)
+a.penup()
+a.forward(150)
+a.pendown()
+
+
+
+
+
+for i in range(3):
+    a.right(120)
+    a.forward(100)
+
+
+
+
+for i in range(3):
+    a.right(120)
+    a.forward(100)
+
+for i in range(3):
+    a.right(120)
+    a.forward(100)
+
+for i in range(3):
+    a.right(120)
+    a.forward(100)
+
+
+
+
+ 
+
+
+
+
+    
+
+
+
---
+++ day1/day1.py
@@ -0,0 +1,58 @@
+import turtle 
+
+
+
+
+
+#spiral = turtle.Turtle()
+
+#for i in range(50):
+ #   spiral.forward(i * 10)
+  #  spiral.right(144)
+    
+#turtle.done()
+
+  
+
+a = turtle.Turtle()
+
+a.forward(100)
+a.right(90) 
+
+a.forward(100)
+a.right(90)
+
+a.forward(100)
+a.right(90)
+
+
+a.forward(100)
+
+a.penup()
+a.forward(50)
+a.right(90)
+a.pendown()
+
+a.forward(150)
+a.right(90)
+
+a.forward(200)
+a.right(90)
+
+a.forward(200)
+
+a.right(90)
+
+
+a.forward(200)
+a.right(90)
+
+
+
+a.forward(50)   
+a.right(90)
+
+turtle.done()
+
+
+
